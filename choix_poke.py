9cbf6d78b8dc2939ef1dbcb6ee9ed6fdfb85c47a
jorisclv <joris.calventus@gmail.com>
2023-10-16 12:08:04 +0200

    m

--- choix_poke.py
+++ choix_poke.py
@@ -17,20 +17,11 @@ def dic2poke(dic):
     "transforme le dic en pokemoon"
     return Pokemoon(dic["nom"], dic["pv"], dic["type_feu"], dic["type_eau"], dic["type_plante"])
 
-def choix_perso1():
+def choix_perso():
     "Déf permettant grace a un input de choisir son pokemoon pour le j1"
     choix_perso = int(input("salameche, darrumarond, carapuce, moustillon, bulbizzare, vipelierre "))
     while choix_perso <= 0 or choix_perso >= 7 :
         choix_perso = int(input("salameche, darrumarond, carapuce, moustillon, bulbizzare, vipelierre "))
     choix_perso -= 1
-    p1 = dic2poke(get_data_poke(choix_perso))
-    return p1
-
-def choix_perso2():
-    "Déf permettant grace a un input de choisir son pokemoon pour le j2"
-    choix_perso = int(input("salameche, darrumarond, carapuce, moustillon, bulbizzare, vipelierre "))
-    while choix_perso <= 0 or choix_perso >= 7 :
-        choix_perso = int(input("salameche, darrumarond, carapuce, moustillon, bulbizzare, vipelierre "))
-    choix_perso -= 1
-    p2 = dic2poke(get_data_poke(choix_perso))
-    return p2
+    p = dic2poke(get_data_poke(choix_perso))
+    return p
